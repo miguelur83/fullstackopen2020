8e03bb6ed4c0076c71153f8167635682cc8eea99
Miguel Ur <miguelur@gmail.com>
2020-11-08 15:35:29 -0300

    2.11 phonebook done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
-  
+
+  useEffect (() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        console.log(response.data)
+    })
+  }, [])
+    
   const handleOnSubmit = (event) => {
     event.preventDefault()
     const newPerson = {
