ed22ea54ebe12f920e3c2def66a51ae6137f5e1b
Miguel Urdinola <42473623+miguelur83@users.noreply.github.com>
2020-11-16 18:09:42 -0300

    All done up to Exercise 2.18
    Including notes example app

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState , useEffect } from 'react'
 import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
   const [ persons, setPersons ] = useState([]) 
@@ -23,12 +24,45 @@ const App = () => {
       name: newName,
       number: newNumber
     }
+    console.log('button clicked', event.target)
     if (persons.findIndex(person => person.name === newName) !== -1) {
       console.log("FOUND");
-      alert (`${newName} is already added to phonebook`)
+      var theperson = persons.find((person) => person.name === newName)
+      if (newNumber != theperson.number){
+        if (window.confirm(`${newName} is already added to phonebook. Do you want to update their number?`)) { 
+          theperson.number = newNumber
+          personService
+          .update(theperson.id, theperson)
+          .then(response => {
+            setPersons(persons.map((person) => (person.id === theperson.id?theperson:person)))
+            console.log(response)
+          })
+        }
+      } else {
+        alert (`${newName} is already added to phonebook`)
+      }
     } else {
-      setPersons(persons.concat(newPerson))
-      console.log('button clicked', event.target)
+      console.log("Saving person");
+      personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+    }
+  }
+
+  const handleDelete = (event) => {
+    var person = persons.find(person => person.id == event.target.id)
+    if (window.confirm(`Do you really want to delete ${person.name}?`)) { 
+      console.log("Delete ", event.target.id);
+      personService
+      .erase(event.target.id)
+      .then(response => {
+        console.log(response);
+        setPersons(persons.filter(person => person.id != event.target.id))
+      })
     }
   }
 
@@ -42,7 +76,6 @@ const App = () => {
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
-
   }
 
   return (
@@ -52,7 +85,7 @@ const App = () => {
       <h3>Add a new person</h3>
       <PersonForm handleOnSubmit={handleOnSubmit} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h3>Numbers</h3>
-      <Persons persons={persons} filter={filter} />
+      <Persons persons={persons} filter={filter} handleDelete={handleDelete} />
     </div>
   )
 }
@@ -81,19 +114,19 @@ const PersonForm = (props) => {
   )
 }
 
-const Persons = ({persons, filter}) => {
+const Persons = ({persons, filter, handleDelete}) => {
   return (
     <ul>
       {persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())).map(person =>
-        <Person key={person.name} person={person} />
+        <Person key={person.name} person={person} handleDelete={handleDelete} />
       )}
     </ul>
   )
 }
 
-const Person = ({person}) => {
+const Person = ({person, handleDelete}) => {
   return(
-    <li key={person.name}>{person.name}: {person.number}</li>
+    <li key={person.name}>{person.name}: {person.number} <button id={person.id} onClick={handleDelete}>delete</button></li>
   )
 }
 
